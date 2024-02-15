befe64ee367a928909b56af2283fdd6241ccae80
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-02-15 20:08:31 +0100

    Version 3

---
+++ src/main/java/pl/asilver/calc/_main/_Main.java
@@ -0,0 +1,13 @@
+package pl.asilver.calc._main;
+import pl.asilver.calc.util.Calculator;
+import static pl.asilver.calc.service.ResultWriteService.writeResult;
+public class _Main {
+    public static void main(String[] args) {
+        int sumOperation = Calculator.findSum(211, 35);
+        int difOperation = Calculator.findDif(123, 321);
+        int multiplOperation = Calculator.findMultipl(40, 222);
+        writeResult(211, 35, sumOperation, "sum");
+        writeResult(123, 321, difOperation, "difference");
+        writeResult(40, 222, multiplOperation, "multiplication");
+    }
+}
--- src/main/java/pl/asilver/calc/util/Calculator.java
+++ src/main/java/pl/asilver/calc/util/Calculator.java
@@ -2,19 +2,12 @@ package pl.asilver.calc.util;
 
 public class Calculator {
     public static int findSum(int a, int b){
-        int sum = a + b;
-        return sum;
+       return a+b;
     }
-
     public static int findDif(int c, int d){
-        int difference = c - d;
-        return difference;
+        return c-d;
     }
-
-    public static int findMultipl(int e, int f){
-        int multiplication = e*f;
-        return multiplication;
+    public static int findMultipl(int e, int f) {
+        return e * f;
     }
-
-
 }
