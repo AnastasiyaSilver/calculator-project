8a9c2357d09edd65cdbdc5f604298e9836327126
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-02-18 12:17:59 +0100

    Version 5

--- src/main/java/pl/asilver/calc/_main/_Main.java
+++ src/main/java/pl/asilver/calc/_main/_Main.java
@@ -1,6 +1,9 @@
 package pl.asilver.calc._main;
+
 import pl.asilver.calc.util.Calculator;
+
 import static pl.asilver.calc.service.ResultWriteService.writeResult;
+
 public class _Main {
     public static void main(String[] args) {
         int sumOperation = Calculator.findSum(211, 35);
