ebe2a07c076d25028a29d4688d50009170f5614b
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-02-18 12:17:20 +0100

    Version 5

--- src/main/java/pl/asilver/calc/util/Calculator.java
+++ src/main/java/pl/asilver/calc/util/Calculator.java
@@ -1,12 +1,12 @@
 package pl.asilver.calc.util;
 
 public class Calculator {
-    public static int findSum(int a, int b){
-       return a+b;
+    public static int findSum(int a, int b) {
+        return a + b;
     }
 
-    public static int findDif(int c, int d){
-        return c-d;
+    public static int findDif(int c, int d) {
+        return c - d;
     }
 
     public static int findMultipl(int e, int f) {
